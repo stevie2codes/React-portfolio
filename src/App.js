d4db71623e1307dc542856ac2c83ded5879c6af0
stevie2codes <stevie2codes@gmail.com>
2020-03-21 08:47:00 -0500

    setup files and begain layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavBar from "./components/nav";
+import Home from "./components/main";
+import About from "./components/about";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
     </div>
   );
 }
