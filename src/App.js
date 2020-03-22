d71129e81827a01afcee1861ef108e0f9e6c8b21
stevie2codes <stevie2codes@gmail.com>
2020-03-22 16:26:22 -0500

    hooked up nav routes and started home page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React from "react";
 import "./App.css";
-import NavBar from "./components/nav";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import Home from "./components/main";
-import About from "./components/about";
 import Projects from "./components/projects";
 import Contact from "./components/contact";
+import About from "./components/about";
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
+      <BrowserRouter>
+        <div className="nav nav-pills my-3 d-flex justify-content-center">
+          <div className="navigation">
+            {/* <img src={logo} className="logo" alt="Logo Image" /> */}
+            <div className="nav-items d-flex">
+              <Link to="/" className="nav-link  m-2">
+                Home
+              </Link>
+              <Link to="/About" className="nav-link  m-2">
+                About
+              </Link>
+              <Link to="/Projects" className="nav-link  m-2">
+                Projects
+              </Link>
+              <Link to="/Contact" className="nav-link  m-2">
+                Contact
+              </Link>
+            </div>
+          </div>
+        </div>
+        <Route exact path="/" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Projects" component={Projects} />
+        <Route path="/Contact" component={Contact} />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Home(props) {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <React.Fragment>
+      <motion.div
+        initial={{ scale: 0 }}
+        className="nameContainer"
+        animate={{ rotate: 360, scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20
+        }}
+      />
+      ;<h1 className="firstName">Stephen Webb</h1>
+    </React.Fragment>
   );
 }
 
