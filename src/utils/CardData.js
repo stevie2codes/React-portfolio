66e37d05b7b20f26b2f4766bf9f7823962eb8043
stevie2codes <stevie2codes@gmail.com>
2020-06-25 16:53:20 -0500

    removed local images and moved them all into cloudinary

--- src/utils/CardData.js
+++ src/utils/CardData.js
@@ -4,7 +4,7 @@ const CardData = [
     description: `A Full-Stack React application allowing use of premade CSS
     animations or use the internal IDE to create, save and update new
     animations.`,
-    image: require("../components/projectPics/homePreview.png"),
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1593121590/ProjectPreviews/Animade_e622wp.png`,
     githubLink: `https://github.com/stevie2codes/Animade`,
     siteLink: `https://animade.herokuapp.com/`
   },
@@ -12,7 +12,7 @@ const CardData = [
     title: "Atlas Photo",
     description: ` A Photographers portfolio built on React and deployed with AWS.
     Utilizing Cloudfront , S3 buckets,route53 and Cloudinary `,
-    image: require("../components/projectPics/atlas.png"),
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1593121365/ProjectPreviews/atlas_caolv8.png`,
     githubLink: `https://github.com/stevie2codes/JMG-Photo`,
     siteLink: `https://atlas-photo.com/`
   },
@@ -20,7 +20,7 @@ const CardData = [
     title: "Outer Earth",
     description: ` A space exploration app utilizing the NASA API. A team project
     where I completed the full design.`,
-    image: require("../components/projectPics/outerEarth.png"),
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1593119623/ProjectPreviews/outerEarth_ov0p7v.png`,
     githubLink: `https://github.com/koganp42/Outer_Earth`,
     siteLink: `https://koganp42.github.io/Outer_Earth/`
   },
@@ -28,14 +28,14 @@ const CardData = [
     title: "Nutri-Find",
     description: ` A recipe keeper saving recipes in a database and searching a 3rd
     party API to find new ones!`,
-    image: require("../components/projectPics/nutriPreview.png"),
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1593120673/ProjectPreviews/nutrifind_o0gpnd.png`,
     githubLink: `https://github.com/stevie2codes/Nutri-Find`,
     siteLink: `https://calm-woodland-11785.herokuapp.com/`
   },
   {
     title: "Employee-Directory",
     description: ` A react application utilizing pagination, filtering and sorting.`,
-    image: require("../components/projectPics/preview.png"),
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1593121715/ProjectPreviews/directory_xpcepo.png`,
     githubLink: `https://github.com/stevie2codes/React-Employee-Directory`,
     siteLink: `https://fast-mesa-07521.herokuapp.com/`
   },
@@ -43,7 +43,7 @@ const CardData = [
     title: "Weather Dashboard",
     description: `Weather application calling out to 3rd party API w/ AJAX and
     utilizing jQuery for DOM manipulation.`,
-    image: require("../components/projectPics/WD-Shot.png"),
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1593121260/ProjectPreviews/weather_xsw3li.png`,
     githubLink: `https://github.com/stevie2codes/Weather-Dashboard`,
     siteLink: `https://stevie2codes.github.io/Weather-Dashboard/`
   },
@@ -51,7 +51,7 @@ const CardData = [
     title: "What the health?",
     description: ` A timed health quiz utilizing jquery DOM manipulation and local
     storage.`,
-    image: require("../components/projectPics/healthQuiz.png"),
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1593120932/ProjectPreviews/whatTheHealth_ix9gwc.png`,
     githubLink: `https://github.com/stevie2codes/health-quiz`,
     siteLink: `https://stevie2codes.github.io/health-quiz/`
   },
@@ -61,7 +61,7 @@ const CardData = [
     Sequelize . Following the MVC design pattern; using Node and MySQL
     to query and route data into the app, and Handlebars to generate
     HTML`,
-    image: require("../components/projectPics/burger.png"),
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1593121039/ProjectPreviews/eatDaburg_sjotxx.png`,
     githubLink: `https://github.com/stevie2codes/Burger-Log`,
     siteLink: `https://warm-brook-07437.herokuapp.com/`
   }
