77642c61c54c4ff93d07efa6b88e383c7c92989d
stevie2codes <stevie2codes@gmail.com>
2020-06-26 13:21:27 -0500

    added hamburger menu and created a seperate navigation component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import Projects from "./components/projects/projects";
-import Contact from "./components/contact/contact";
-import About from "./components/about/about";
-import HomeContent from "./components/home/homeContent";
+
+import Navigation from "./components/Navigation";
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <div className="nav nav-pills d-flex justify-content-end">
-          <div className="navigation">
-            <Link to="/">
-              <h3 className="logo">W</h3>
-            </Link>
-
-            <div className="nav-items d-flex">
-              <Link to="/" className="nav-link ">
-                Home
-              </Link>
-              <Link to="/About" className="nav-link ">
-                About
-              </Link>
-              <Link to="/Projects" className="nav-link ">
-                Projects
-              </Link>
-              <Link to="/Contact" className="nav-link ">
-                Contact
-              </Link>
-            </div>
-          </div>
-        </div>
-
-        <Route exact path="/" component={HomeContent} />
-        <Route path="/About" component={About} />
-        <Route path="/Projects" component={Projects} />
-        <Route path="/Contact" component={Contact} />
-      </BrowserRouter>
-    </div>
-  );
+  return <Navigation />;
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Projects from "./projects/projects";
+import Contact from "./contact/contact";
+import About from "./about/about";
+import HomeContent from "./home/homeContent";
+import "./styles.css/navigation.css";
+const onOpen = () => {
+  const navLinks = document.querySelector(".navigation");
+  navLinks.classList.toggle("open");
+};
+function Navigation(props) {
+  return (
+    <div>
+      <BrowserRouter>
+        <Link to="/">
+          <h3 className="logo">W</h3>
+        </Link>
+        <div className="nav nav-pills d-flex">
+          <div onClick={() => onOpen()} className="hamburger">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+          <div className="navigation">
+            <div className="nav-items d-flex">
+              <Link onClick={() => onOpen()} to="/" className="nav-link ">
+                Home
+              </Link>
+              <Link onClick={() => onOpen()} to="/About" className="nav-link ">
+                About
+              </Link>
+              <Link
+                onClick={() => onOpen()}
+                to="/Projects"
+                className="nav-link "
+              >
+                Projects
+              </Link>
+              <Link
+                onClick={() => onOpen()}
+                to="/Contact"
+                className="nav-link "
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <Route exact path="/" component={HomeContent} />
+        <Route path="/About" component={About} />
+        <Route path="/Projects" component={Projects} />
+        <Route path="/Contact" component={Contact} />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default Navigation;
