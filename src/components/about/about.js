d27b95c56ca1f7a0ae1dc1b221323014fc97d3dc
stevie2codes <stevie2codes@gmail.com>
2020-06-26 14:11:00 -0500

    removing fade in component to see if it conflicts with nav menu on build

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import FadeIn from "react-fade-in";
+
 function About(props) {
   return (
     <React.Fragment>
@@ -10,34 +10,33 @@ function About(props) {
         className="portPic"
         alt="pic"
       />
-      <FadeIn transitionDuration={1000}>
-        <div className="container">
-          <div className="aboutText row ">
-            <p className="brand-state col-md-6 ">
+
+      <div className="container">
+        <div className="aboutText row ">
+          <p className="brand-state col-md-6 ">
+            <span className="aboutSpan d-flex justify-content-center">
+              <span>About Me</span>
+            </span>
+            Front-End Web Developer, husband, father and runner. A background in
+            business ownership and a passion for creative endeavors. Studied
+            full-stack web development at Vanderbilt University where I excelled
+            in UI Design. Diving deep into this field of development, I'm
+            excited for a life long journey into learning new technologies
+            daily!
+          </p>
+          <div className="list col-md-6">
+            <p className="skillList">
               <span className="aboutSpan d-flex justify-content-center">
-                <span>About Me</span>
+                <span>Studied Skills</span>
               </span>
-              Front-End Web Developer, husband, father and runner. A background
-              in business ownership and a passion for creative endeavors.
-              Studied full-stack web development at Vanderbilt University where
-              I excelled in UI Design. Diving deep into this field of
-              development, I'm excited for a life long journey into learning new
-              technologies daily!
+              MERN STACK- Mongo, Express,React,React Native, Node, CSS, SASS,
+              bootstrap, JavaScript animaition libraries, JavaScript, jQuery,
+              MySQL, Mongoose,AWS, git, github, MVC Agile,SCRUM and team
+              projects. Figma Design Sytems . Ui/Ux Design principles.
             </p>
-            <div className="list col-md-6">
-              <p className="skillList">
-                <span className="aboutSpan d-flex justify-content-center">
-                  <span>Studied Skills</span>
-                </span>
-                MERN STACK- Mongo, Express,React,React Native, Node, CSS, SASS,
-                bootstrap, JavaScript animaition libraries, JavaScript, jQuery,
-                MySQL, Mongoose,AWS, git, github, MVC Agile,SCRUM and team
-                projects. Figma Design Sytems . Ui/Ux Design principles.
-              </p>
-            </div>
           </div>
         </div>
-      </FadeIn>
+      </div>
     </React.Fragment>
   );
 }
